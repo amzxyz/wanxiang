e5f7db6157190091eae69d6ec31ca1c77b371107
domking <beixianchen@gamil.com>
2024-12-30 10:00:12 +0800

    更新字符过滤函数，新增词频筛选功能并调整输出文件

--- cn_dicts/chars_filter.py
+++ cn_dicts/chars_filter.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-def filter_chars():
+def filter_chars(frequency: int = 1):
     # 使用相对路径
     base_dir = Path(__file__).parent
     origin_file = base_dir / "chars.dict.yaml"
-    dest_file = base_dir / "chars.simple.dict.yaml"
+    universal_file = base_dir / "chars.universal.dict.yaml"
+    frequency_file = base_dir / "chars.frequency.dict.yaml"
     simple_file = base_dir / "8105.dict.yaml"
 
     try:
@@ -23,11 +24,22 @@ def filter_chars():
         ]
         
         # 将筛选后的结果写入 destFile
-        with dest_file.open("w", encoding="utf-8") as df:
+        with universal_file.open("w", encoding="utf-8") as df:
             df.writelines(filtered_lines)
+
+        # origin_file 中 词频大于 frequency 的行
+        filtered_lines = [
+            line for line in lines 
+            if "\t" not in line or int(line.split("\t")[2].strip()) > frequency
+        ]
+
+        # 写入frequency_file
+        with frequency_file.open("w", encoding="utf-8") as df:
+            df.writelines(filtered_lines)
+        
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # 调用方法
 if __name__ == "__main__":
-    filter_chars()
+    filter_chars(1)
